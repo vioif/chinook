717d114f99e08f9027208000bf231d479f3bfbaf
vioif <ireneaflores3@gmail.com>
2018-01-15 17:24:30 -0500

    part 1 task 1-4 completed

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,59 @@
 class Track < ApplicationRecord
+
+  SHORT = 180000
+  LONG  = 360000
+
+  scope :short,  -> { shorter_than(SHORT) }
+  scope :medium, -> { shorter_than(LONG).longer_than_or_equal_to(SHORT) }
+  scope :long,   -> { longer_than_or_equal_to(LONG) }
+  scope :starts_with, -> (char) { where('name ILIKE ?', "#{ char }%") }
+
+
+
+
+
+  # scope looks into code and shortens code into one line
+  #
+  # def self.short
+  #   where('milliseconds < ?', SHORT)
+  # end
+  #
+  # def self.medium
+  #   where('milliseconds < ?', SHORT)
+  #     .where('milliseconds < ?', LONG)
+  # end
+  #
+  # def self.long
+  #   where('milliseconds >= ?', LONG)
+  # end
+  #
+  # def self.shorter_than(milliseconds)
+  #   if milliseconds && milliseconds >= 0
+  #     where('milliseconds < ?', milliseconds)
+  #   else # track.all means the same as all
+  #     all
+  #   end
+  # end
+  # SAME AS -------------->
+  scope :shorter_than, ->(milliseconds) {
+    where('milliseconds < ?', milliseconds) if milliseconds && milliseconds >= 0
+  
+  # def self.longer_than_or_equal_to(milliseconds)
+  #   if milliseconds && milliseconds >= 0
+  #     where('milliseconds >= ?', milliseconds)
+  #   else
+  #     all
+  #   end
+  # end
+  # SAME AS-------------->
+  scope :longer_than, ->(milliseconds) {
+     where('milliseconds >= ?', milliseconds) if milliseconds && milliseconds >= 0
+  }
+
+
+  # def self.starts_with(char)
+  #   where('name ILIKE ?', "#{ char }%")
+  # end
+
+
 end
